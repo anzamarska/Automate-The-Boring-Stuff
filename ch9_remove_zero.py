b05cb3c18bed1f3c6159b6c345e44b89c38d85cf
anzamarska <anzamarska@gmail.com>
2018-12-29 12:19:11 +0100

    ?

---
+++ ch9_remove_zero.py
@@ -0,0 +1,22 @@
+import shutil, os, re
+
+zero = re.compile(r'''^(.*?)
+(0*)
+(.*?)$
+''')
+
+for oldFileName in os.listdir('.'):
+    mo = zero.search(oldFileName)
+    if mo == None:
+        continue
+    beforePart = mo.group(1)
+    zerosPart = mo.group(2)
+    afterPart = mo.group(3)
+    newName= beforePart + afterPart
+
+    absWorkingDir= os.path.abspath('.')
+    oldFileName=os.path.join(absWorkingDir, oldFileName)
+    newFileName=os.path.join(absWorkingDir, newName)
+    
+    print ('Renaming "%s" to "%s"...' % (oldFileName, newFileName))
+    #shutil.move(oldFileName, newFileName)
